d7934ec653fbaa214706693eeb4c7886e9443a20
mixx99 <maxkriv1@gmail.com>
2025-04-20 14:01:22 +0300

    pipeline updated

--- src/lve_pipeline.cpp
+++ src/lve_pipeline.cpp
@@ -4,8 +4,11 @@
 #include <stdexcept>
 #include <iostream>
 namespace lve{
-  LvePipeline::LvePipeline(const std::string& vertFilePath, const std::string& fragFilePath){
-    createGraphicsPipeline(vertFilePath, fragFilePath);
+  LvePipeline::LvePipeline(LveDevice& device, const std::string& vertFilePath, const std::string& fragFilePath, const PipelineConfigInfo& configInfo)
+  :
+  lveDevice(device)
+  {
+    createGraphicsPipeline(vertFilePath, fragFilePath, configInfo);
   }
 
   std::vector<char> LvePipeline::readFile(const std::string& filepath){
@@ -21,11 +24,25 @@ namespace lve{
     file.close();
     return buffer;
   }
-  void LvePipeline::createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath){
+  void LvePipeline::createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath, const PipelineConfigInfo& configInfo){
     auto vertCode = readFile(vertFilePath);
     auto fragCode = readFile(fragFilePath);
 
     std::cout << "Vertex shader code size = " << vertCode.size() << std::endl;
     std::cout << "Frag shader code size = " << fragCode.size() << std::endl;
   }
+  void LvePipeline::createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule){
+    VkShaderModuleCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.codeSize = code.size();
+    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+    if(vkCreateShaderModule(lveDevice.device(), &createInfo, nullptr, shaderModule) != VK_SUCCESS){
+      throw std::runtime_error("Faild to create shader module");
+    }
+  }
+  PipelineConfigInfo LvePipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height){
+    PipelineConfigInfo configInfo{};
+
+    return configInfo;
+  } 
 }
--- src/lve_pipeline.hpp
+++ src/lve_pipeline.hpp
@@ -1,14 +1,30 @@
 #pragma once
 
+#include "lve_device.hpp"
+
+// std
 #include <string>
 #include <vector>
 namespace lve{
+  
+  struct PipelineConfigInfo{
+
+  };
   class LvePipeline{
+    LveDevice& lveDevice;
+    VkPipeline graphicsPipeline;
+    VkShaderModule vertShaderModule;
+    VkShaderModule fragShaderModule;
   public:
-    LvePipeline(const std::string& vertFilePath, const std::string& fragFilePath);
+    LvePipeline(LveDevice& device, const std::string& vertFilePath, const std::string& fragFilePath, const PipelineConfigInfo& configInfo);
+    ~LvePipeline(){}
+    void operator=(const LvePipeline&) = delete;
+    static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
+
     
   private:
     static std::vector<char> readFile(const std::string& filepath);
-    void createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath);
+    void createGraphicsPipeline(const std::string& vertFilePath, const std::string& fragFilePath, const PipelineConfigInfo& configInfo);
+    void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
   };
 }
